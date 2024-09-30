0978d75b2470b44b0dfc26b9d8c7748696f0c032
Jefeson <jefesonvs@hotmail.com>
2024-09-30 15:41:10 +0100

    Adding classes generated from StarUML

---
+++ code/Game.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame
+
+from MountainShooter.code.Menu import Menu
+from MountainShooter.code.const import WIN_WIDTH, WIN_HEIGHT
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
